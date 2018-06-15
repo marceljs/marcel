e9f4a1751a90aaf29cc1f3319ff563731b80617e
Dan Burzo <danburzo@gmail.com>
2018-06-15 18:20:00 +0300

    Add default config file

--- src/config.js
+++ src/config.js
@@ -5,9 +5,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const DEFAULT_CONFIG_FILE_PATH = 'marcel.config.js';
+const default_config = require('./defaults/default.config.js');
+
+const CONFIG_FILE_DEFAULT_PATH = 'marcel.config.js';
 
 module.exports = bundler => {
-	const config_path = path.join(bundler.base, DEFAULT_CONFIG_FILE_PATH);
-	return require(config_path);
+	const config_path = path.join(bundler.base, CONFIG_FILE_DEFAULT_PATH);
+	return {
+		...default_config,
+		...require(config_path)
+	};
 };
---
+++ src/defaults/default.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	contentDir: 'content',
+	dataDir: 'data',
+	templateDir: 'templates',
+	staticDir: 'static',
+	distDir: 'dist'
+};
