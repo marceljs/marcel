f5c96f9594fe2159aed763c6339886e8fc2ca6f1
Dan Burzo <danburzo@gmail.com>
2018-06-17 01:09:34 +0300

    test util/group-by

---
+++ test/util/group-by.test.js
@@ -0,0 +1,27 @@
+const tape = require('tape');
+const groupby = require('../../src/util/group-by');
+
+tape('test section grouping', test => {
+	let posts = [{ id: 1, section: 'a' }, { id: 2, section: 'b' }, { id: 3, section: 'a' }];
+
+	test.deepEqual(
+		groupby(posts, post => post.section),
+		{
+			a: [{ id: 1, section: 'a' }, { id: 3, section: 'a' }],
+			b: [{ id: 2, section: 'b' }]
+		},
+		'group by section'
+	);
+
+	test.deepEqual(
+		groupby(posts, post => post.id),
+		{
+			1: [{ id: 1, section: 'a' }],
+			2: [{ id: 2, section: 'b' }],
+			3: [{ id: 3, section: 'a' }]
+		},
+		'group by id'
+	);
+
+	test.end();
+});
